2536481d5156a8437d5bdf319a3245fe8512bc36
Désirée Lussier <9257980+dllussier@users.noreply.github.com>
2018-10-02 10:46:49 -0400

    Update hsFEAT.sh

--- scriptsdir/hsFEAT.sh
+++ scriptsdir/hsFEAT.sh
@@ -6,7 +6,7 @@
 #SBATCH --mail-user=<email_associated_with_hipergator_account>
 #SBATCH --ntasks=1
 #SBATCH --mem=4gb
-#SBATCH --time=12:00:00
+#SBATCH --time=6:00:00
 #SBATCH --output=hsfeat_%j.out
 pwd; hostname; date
 
